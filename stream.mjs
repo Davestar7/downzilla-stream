c8d32667d852788dd28c86525a9e98df587f77f1
Davestar7 <davidnyeokpobiyadavid3633@gmail.com>
2026-05-24 17:52:27 +0100

    Update stream.mjs

--- stream.mjs
+++ stream.mjs
@@ -8,6 +8,8 @@ const origin = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
     "https://downzilla.netlify.app",
+    "https://www.downzilla.buzz",
+    "https://downzilla.buzz"
     ]
 app.use(cors({
     origin: origin,
