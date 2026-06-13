00cf1d9aaaca89c92eb8e66145ff56bfe7d27477
Davestar7 <davidnyeokpobiyadavid3633@gmail.com>
2026-06-13 22:21:35 +0100

    Replace argument array with dynamic argument list

--- operation/getMetadata.mjs
+++ operation/getMetadata.mjs
@@ -43,7 +43,7 @@ const metadataExtractor = async (req, res) => {
             
             if (type === "video") {
               const argss = ['--cookies', cookie, '--js-runtimes', 'node','-j','--no-warnings', '--skip-download', '--no-check-certificate', '--no-playlist','--force-ipv4',  '--retries', 'infinite','--fragment-retries', 'infinite','--ignore-errors','--no-cache-dir', url];
-              proc = spawn(ytDlpPath, argss, { stdio: ["ignore", "pipe", "pipe"] })
+              proc = spawn(ytDlpPath, argument, { stdio: ["ignore", "pipe", "pipe"] })
             
              // proc = spawn(ytDlpPath, ['--cookies', cookie, '-j', '-S', '+size,+br', '--no-warnings', '--skip-download', '--no-check-certificate', '--no-playlist', '--force-ipv4', '--retries', 'infinite', '--fragment-retries', 'infinite', '--ignore-errors', '--no-cache-dir', url], { stdio: ["ignore", "pipe", "pipe"] })
               //proc = spawn(ytDlpPath, ['--cookies', cookie, '-j', '--skip-download', '--no-check-certificate', '--no-playlist', '--retries', 'infinite', '--fragment-retries', 'infinite', '--ignore-errors', '--no-cache-dir', '--extractor-args', 'youtube:player_client=android_vr,tv', url], { stdio: ["ignore", "pipe", "pipe"] })
