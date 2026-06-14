0520243110a95ac4af8752ad6943c27b839beded
Davestar7 <davidnyeokpobiyadavid3633@gmail.com>
2026-06-14 10:03:07 +0100

    Update yt-dlp arguments for video processing

--- operation/getMetadata.mjs
+++ operation/getMetadata.mjs
@@ -43,7 +43,7 @@ const metadataExtractor = async (req, res) => {
             
             if (type === "video") {
               //const argss = ['--cookies', cookie, '--js-runtimes', 'node','-j','--no-warnings', '--skip-download', '--no-check-certificate', '--no-playlist','--force-ipv4',  '--retries', 'infinite','--fragment-retries', 'infinite','--ignore-errors','--no-cache-dir', url];
-              const argss = ['--verbose', '--cookies', cookie, '--js-runtimes', 'node', '--no-warnings', '--skip-download', '--no-check-certificate', '--no-playlist', '--force-ipv4', '--retries', 'infinite', '--fragment-retries', 'infinite', '--no-cache-dir', '--extractor-args', 'youtube:player_client=web', '-J', url];
+              const argss = ['--verbose', '--ignore-config', '--cookies', cookie, '--js-runtimes', 'node', '--no-warnings', '--skip-download', '--no-check-certificate', '--no-playlist', '--force-ipv4', '--retries', 'infinite', '--fragment-retries', 'infinite', '--no-cache-dir', '--extractor-args', 'youtube:player_client=web', '-J', url];
               proc = spawn(ytDlpPath, argss, { stdio: ["ignore", "pipe", "pipe"] })
             
              // proc = spawn(ytDlpPath, ['--cookies', cookie, '-j', '-S', '+size,+br', '--no-warnings', '--skip-download', '--no-check-certificate', '--no-playlist', '--force-ipv4', '--retries', 'infinite', '--fragment-retries', 'infinite', '--ignore-errors', '--no-cache-dir', url], { stdio: ["ignore", "pipe", "pipe"] })
